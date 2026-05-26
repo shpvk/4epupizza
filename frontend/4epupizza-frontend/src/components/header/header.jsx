2f8efc8d71eebb7c362ec17c10c753d75c8b46a4
artem <maloyartem07@gmail.com>
2026-05-26 23:49:37 +0300

    Make skelet of header,add fonts,and images

---
+++ frontend/4epupizza-frontend/src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className='header_logo'>
+                <a><img src="/img/4epupizza.png" /></a>
+            </div>
+            <div className='nav_bar'>
+                <nav>
+                    <ul>
+                        <li>Пицца</li>
+                        <li>Конструктор</li>
+                        <li>Акции</li>
+                        <li>Про нас</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className='header_actions'>
+                <button className="btn--login">Login</button>
+                <button className="btn--cart"><img src='/img/Group 3.png'></img></button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
