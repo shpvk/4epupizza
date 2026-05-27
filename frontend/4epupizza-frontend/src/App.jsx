13c5a4c3d0bd368b0e1950a9727e4805c6e36fe6
shpvk <138403371+shpvk@users.noreply.github.com>
2026-05-27 23:20:09 +0300

    чистка меток от слияния веток

--- frontend/4epupizza-frontend/src/App.jsx
+++ frontend/4epupizza-frontend/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import CheckoutPage from './components/checkout/CheckoutPage'
-=======
-import Footer from './components/footer/footer'
 
 function App() {
   const isCheckoutPage = window.location.pathname === '/checkout'
@@ -13,10 +11,6 @@ function App() {
       <Header />
       <main>
         {isCheckoutPage && <CheckoutPage />}
-      </main>
-      <Footer />
-      <main>
-        {/* Main content will go here */}
       </main>
       <Footer />
     </>
