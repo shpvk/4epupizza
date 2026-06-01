65555ebf77b9b53b4c0ef0ddf55111092b462a97
shpvk <138403371+shpvk@users.noreply.github.com>
2026-06-01 15:20:08 +0300

    Create the base for the order

---
+++ backend/ChepuPizza.BLL/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using ChepuPizza.BLL.DTO;
+
+namespace ChepuPizza.BLL.Services
+{
+    public class OrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+
+
+
+        public async Task<OrderResponse> CreateAsync(OrderRequest request)
+        {
+
+        }
+    }
+}
