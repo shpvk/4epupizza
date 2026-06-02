440a709043da499b0364afbce55776a7c3aa8def
shpvk <138403371+shpvk@users.noreply.github.com>
2026-06-02 17:41:51 +0300

    Restore frontend from artem#7 branch

---
+++ frontend/4epupizza-frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+import Header from '../components/header/header'
+import Footer from '../components/footer/footer'
+import Main from '../components/Main/Main'
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
+
