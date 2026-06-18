1456d6d31ab9f2d0f1b60fc17240523d29552896
shpvk <138403371+shpvk@users.noreply.github.com>
2026-06-18 21:14:09 +0300

    Created the foundation for JWT authentication

---
+++ backend/ChepuPizza.BLL/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using ChepuPizza.BLL.DTO;
+
+namespace ChepuPizza.BLL.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> RegisterAsync(RegisterUserRequest request);
+    }
+}
---
+++ backend/ChepuPizza.BLL/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using ChepuPizza.BLL.DTO;
+using ChepuPizza.BLL.Interfaces;
+using ChepuPizza.DAL.Models.Entities;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+
+namespace ChepuPizza.BLL.Services
+{
+    public class AuthService : IAuthService
+    {
+        public async Task<AuthResponse> RegisterAsync(RegisterUserRequest request)
+        {
+            (User? user, string? error) = User.Create(request.Username, request.Password);
+            if(user == null)
+            {
+                throw new Exception(error);
+            }
+
+            var hashedPassword = new PasswordHasher<User>()
+
+
+        }
+    }
+}
