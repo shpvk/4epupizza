b1c1dae59fe5bd0db0f5bf1b21dcf8a768a86e2f
Xamilio <Xamilio25@gmail.com>
2026-06-18 23:15:42 +0300

    add storage

--- frontend/4epupizza-frontend/src/App.jsx
+++ frontend/4epupizza-frontend/src/App.jsx
@@ -4,21 +4,26 @@ import Login from './pages/Login/Login'
 import ForgotPassword from './pages/Login/ForgotPassword'
 import SignUp from './pages/Login/SignUp'
 import Promotions from './pages/Promotions'
+import Cart from './pages/Cart/Cart'
 import PizzaConstructorPage from './components/constructor/PizzaConstructorPage'
+import { CartProvider } from './context/CartContext'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/constructor" element={<PizzaConstructorPage />} />
-        <Route path="/promotions" element={<Promotions />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/constructor" element={<PizzaConstructorPage />} />
+          <Route path="/promotions" element={<Promotions />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   )
 }
 
